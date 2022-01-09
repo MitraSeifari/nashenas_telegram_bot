4b8ced9060ca220ac218735e06834e4fdbb70472
Mitra <seifari.mi@gmail.com>
2022-01-09 13:25:41 -0600

    add base code to connect strangers

--- src/constants.py
+++ src/constants.py
@@ -5,8 +5,16 @@ from src.utils.keyboard import create_keyboard
 keys = SimpleNamespace(
     random_connect=':bust_in_silhouette: Random Connect',
     settings=':gear: Settings',
+    exit=':cross_mark: Exit',
 )
 
 keyboards = SimpleNamespace(
     main=create_keyboard(keys.random_connect, keys.settings),
+    exit=create_keyboard(keys.exit),
+)
+
+states = SimpleNamespace(
+    random_connect='RANDOM_CONNECT',
+    main='MAIN',
+    connected='CONNECTED'
 )
